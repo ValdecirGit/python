cbc54ed5a3c5f0a977153da11f0b6459b78b1783
Valdecir RML <valdecir.rml@gmail.com>
2022-12-29 17:14:03 -0300

    Variaveis e tipos de dados

---
+++ aula1.py
@@ -0,0 +1,18 @@
+nome = "Valdecir Ribeiro de Melo"  #tipo de dados String
+idade = 45                         #tipo de dados int
+altura = 1.74                      #tipo de dados float
+is_male = True                     #tipo de dados boolean 
+programacao = "Python"
+
+print(f'{nome} testando Python')
+print(f'códido {programacao}')
+print(f'Linguagem de programação {programacao}')
+
+nome = "Fulano de tal"
+
+print(f'{nome} fazendo testes com linguagem de programação {programacao}')
+print(f'{nome} tem {idade} anos de idade e mede {altura} de altura')
+
+
+
+
