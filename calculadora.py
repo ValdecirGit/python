9387500ae881671b3812a1b17d4a4ec93313a844
Valdecir RML <valdecir.rml@gmail.com>
2023-01-02 14:54:20 -0300

    Input, Comentários e Calculadora Básica

---
+++ calculadora.py
@@ -0,0 +1,4 @@
+num1 = float(input("digite um número: "))
+num2 = float(input("digite outro número: "))
+resultado = num1 + num2
+print(f"O resultado é: {resultado}")
---
+++ inputsComentarios.py
@@ -0,0 +1,20 @@
+# salva input dentro da  variavel nome
+nome = input ("Digite seu nome:")
+
+# salva input dentro da  variavel nome e tranforma ela pra tipo inteiro
+idade = int(input(f"Qual a sua idade {nome}?"))
+
+ano_nascimento = 2022 - idade
+
+print(f"Seu nome é {nome}, vc tem {idade} anos de idade e nasceu no ano de {ano_nascimento}")
+
+#COMENTÁRIOS
+#------------------------------------------------------------------------------------------------------------------------
+
+# este é um comentário
+
+"""
+este é outro 
+tipo de comentários,
+mas não é uma prática recomendada
+"""
